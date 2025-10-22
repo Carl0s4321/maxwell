c9a5ac9a98ffbe76b53741e81549887abb450037
Carlos Sujanto <carlos.sujanto@ucalgary.ca>
2025-10-21 18:39:00 -0600

    navBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <div className="frame">
+      <h1 className="frame__title">Maxwell the Cat</h1>
+      <div className="frame__links">
+        <a href="#">About</a>
+        <a href="#">More Maxwell</a>
+      </div>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
