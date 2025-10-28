9cc3775e6c3bf79e58a6587dcb1f2d9073904a54
Carlos Sujanto <carlos.sujanto@ucalgary.ca>
2025-10-28 15:54:19 -0600

    tweak the fragment

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -5,15 +5,14 @@ uniform float uDir;
 varying vec2 vUv;
 
 void main() {
-	vec4 distortion = texture2D(uDisp,(vUv - vec2(.5)) );
+	// vUv calc for distort scaling
+	vec4 distortion = texture2D(uDisp, (vUv - vec2(.5) * (1. + uSpeed) + vec2(.5)));
 
 	// distort on the edges then lower on the center
-	float force = pow(abs(vUv.x) + 0.5, abs(uSpeed *0.02));
-	vec2 newUv = vUv * cos(1.-force);
+	float force = pow(abs(vUv.x) + 0.5, abs(uSpeed));
+	vec2 newUv = vUv * cos(1. - force);
 
 	// 0.7 for the fading opacity
-	gl_FragColor = (0.01 + uSpeed )* texture2D(uTexture, newUv + distortion.xy * 0.02 + vec2(0., -0.03));
-
-
+	gl_FragColor = (0.05 + 1.25 * uSpeed) * texture2D(uTexture, newUv + distortion.xy * 0.02 + vec2(0., -0.03));
 
 }
